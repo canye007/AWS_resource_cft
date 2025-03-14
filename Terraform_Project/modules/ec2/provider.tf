c90c6fea91b357ff5d82d9bce05b2dbedbdd76b4
canye007 <canyeaws@gmail.com>
2025-03-14 10:52:01 -0500

    add providedr file

--- Terraform_Project/modules/ec2/provider.tf
+++ Terraform_Project/modules/ec2/provider.tf
@@ -9,5 +9,19 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
+  alias = "east"
   region = "us-east-1"
+  profile = "devops-terraform-project"
+}
+
+provider "aws" {
+  alias = "west"
+  region = "us-west-1"
+  profile = "aws_prog_access_cred"
+}
+
+provider "aws" {
+  alias = "west2"
+  region = "us-west-2"
+  profile = "aws_prog_access_cred"
 }
